e3b3d9e0aefa10163c62484479ecc09c710aea79
BalandinaM <marina.balandina.89@mail.ru>
2025-04-06 21:23:57 +0500

    Стили селекта, кнопки в состоянии дисейбл

--- my-react-app/src/selectCategory/selectCategory.jsx
+++ my-react-app/src/selectCategory/selectCategory.jsx
@@ -1,10 +1,11 @@
 import styles from './selectCategory.module.css'
 
 const SelectCategory = () => {
+
 	return (
 		<div className={styles.wrap}>
 			<h3>Выбери категорию функций по которой устроим проверку:</h3>
-			<select>
+			<select className={styles.select}>
 				<option>Массивы</option>
 				<option>Строки</option>
 				<option>Математические методы</option>
