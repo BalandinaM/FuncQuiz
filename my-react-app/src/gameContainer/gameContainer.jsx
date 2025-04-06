427af35273d1ade83d189bb20ce4a7039aa20ca1
BalandinaM <marina.balandina.89@mail.ru>
2025-04-06 21:24:24 +0500

    Таймер

--- my-react-app/src/gameContainer/gameContainer.jsx
+++ my-react-app/src/gameContainer/gameContainer.jsx
@@ -1,13 +1,66 @@
 import SelectCategory from "../selectCategory/selectCategory";
 import GameField from "../gameField/gameField";
-import styles from './gameContainer.module.css'
+import styles from './gameContainer.module.css';
+import { useState } from "react";
 
 const GameContainer = () => {
+	const [isGameStarted, setIsGameStarted] = useState(false);
+	const [secondsTimer, setSecondsTimer] = useState(60);
+
+	console.log(isGameStarted);
+	// let i = 0;
+	// let timerId = setInterval(() => {
+	// 	console.log(++i);
+
+	// 	if(i >= 60) {
+	// 		clearInterval(timerId)
+	// 	}
+	// }, 1000);
+
+	// if (isGameStarted) {
+
+	// }
+
+	const startGame = () => {
+		setIsGameStarted(true);
+		console.log("игра началась!");
+		let i = secondsTimer;
+		let timerId = setInterval(() => {
+			setSecondsTimer(--i);
+			if (i === 0) {
+				clearInterval(timerId);
+			}
+		}, 1000);
+	};
+	//написать функцию для таймера
+	//наверное понадобится useEffect следящий за состоянием isGameStarted
+	//взможно утром тебе покажется что зря разбила на кучу компонентов так как придется кучу всего передавать через пропсы, но
+	//компоненты должны быть тупенькими, они ни чего не должны занть что происходит,
+	//они отрисовывают контент
+
 	return (
 		<>
 			<SelectCategory />
-			<button className={styles.buttonPlay}>Начать игру!</button>
-			<GameField />
+			<div>
+				Время игры:
+				<select className={styles.select}>
+					<option>1 минута</option>
+					<option>2 минуты</option>
+					<option>3 минуты</option>
+				</select>
+			</div>
+			<button className={styles.buttonPlay} onClick={() => startGame()} disabled={isGameStarted}>
+				Начать
+			</button>
+			{/* <GameField /> */}
+			<div className={styles.wrap}>
+				{/* <div className={styles.timer}>00:00:00</div> */}
+				<div className={styles.timer}>{secondsTimer}</div>
+				<div className={styles.question}>
+					<p>Метод изменяет порядок элементов в массиве на обратный</p>
+				</div>
+				<input className={styles.input} type="text" placeholder="reverse()" />
+			</div>
 		</>
 	);
 };
--- my-react-app/src/root/root.jsx
+++ my-react-app/src/root/root.jsx
@@ -3,17 +3,22 @@ import Footer from '../footer/footer';
 import GameContainer from '../gameContainer/gameContainer';
 import Header from '../header/header';
 import ResultGame from '../resultGame/resultGame';
-import styles from './root.module.css'
+import styles from './root.module.css';
+import { useState } from 'react';
 
-function Root() {
+const Root = () => {
+	const [showGameScreen, setShowGameScreen] = useState(true);//когда таймер дотикает надо будет менять на false
+
 
   return (
     <div className={styles.pageContainer}>
       <Header/>
 			<div className={styles.wrapContent}>
 				<AboutGame />
-				{/* <GameContainer /> */}
-				<ResultGame />
+				{showGameScreen ?
+					<GameContainer /> :
+					<ResultGame />
+				}
 			</div>
 			<Footer/>
     </div>
