f57a5a263004e3daaed963674c75562bfe21e5de
BalandinaM <marina.balandina.89@mail.ru>
2025-04-08 15:30:45 +0500

    отключение валидации пропсов

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      eslint: {
+        // Отключаем проверку prop-types
+        rules: {
+          'react/prop-types': 'off',
+        }
+      }
+    })
+  ]
 })
